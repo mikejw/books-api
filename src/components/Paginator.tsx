61838c5f9428a038692fb878cf9af96bde8a5227
Mike Whiting <mikejw3@gmail.com>
2021-01-29 20:35:17 +0000

    cleanup

--- src/components/Paginator.tsx
+++ src/components/Paginator.tsx
@@ -13,21 +13,14 @@ function Paginator({ page, count, itemsPerPage }: PaginatorProps) {
 
   page = Number(page);
   const pages = Math.ceil(count / itemsPerPage);
-  let items: any[] = [];
-
-  items.push(
-    <Pagination.Prev key={ 0 } disabled={ page === 1 } href={`/${page - 1}`} />
-  );
-  items.push(
-    <Pagination.Item key={ 1 } active>{ page }</Pagination.Item>
-  );
-  items.push(
-    <Pagination.Next key={ 2 } disabled={ page === pages } href={`/${page + 1}`} />
-  );
-
+  
   return (
     <div>
-      <Pagination size="sm">{ items }</Pagination>
+      <Pagination size="sm">
+        <Pagination.Prev disabled={ page === 1 } href={`/${page - 1}`} />
+        <Pagination.Item active>{ page }</Pagination.Item>
+        <Pagination.Next disabled={ page === pages } href={`/${page + 1}`} />
+      </Pagination>
     </div>
   );
 };
