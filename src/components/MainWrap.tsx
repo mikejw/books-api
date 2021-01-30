541f33d974a8a56595c5040a13ff3c482d597690
Mike Whiting <mikejw3@gmail.com>
2021-01-30 15:19:49 +0000

    use history object

--- src/components/MainWrap.tsx
+++ src/components/MainWrap.tsx
@@ -1,10 +1,9 @@
 import React, { FormEvent, ChangeEvent, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import Main from './Main';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
 /*
@@ -19,37 +18,33 @@ interface MainWrapProps {
 }
 
 function MainWrap({ pageReq }: MainWrapProps) {
-  let query = useQuery();
+  const query = useQuery();
+  const history = useHistory();
   const foundSearch = query.get('q')
-  //console.log(foundSearch);
   const [ search, setSearch ] = useState(!!foundSearch? foundSearch: '');
   const [ submittedSearch, setSubmittedSearch] = useState(search);
 
+
+  /* Will not work as expected with use of
+   * `event.preventDefault()` and without
+   * `history.push()`.
+   */
   function submitSearch(event: FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
     pageReq = 1;
     setSubmittedSearch(search);
+    history.push(`/1?q=${ encodeURI(search) }`);
   }
 
   function onChange(event: ChangeEvent<HTMLInputElement>): void {
     setSearch(event.currentTarget.value);
   }
 
-  console.log(pageReq);
-  // use history.push!
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">Great Books</Navbar.Brand>
-
         <Nav className="mr-auto">
-          {/*
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              */}
         </Nav>
-
         <Form method="GET" inline onSubmit={ submitSearch }>
           <Form.Control
             onChange={ onChange }
